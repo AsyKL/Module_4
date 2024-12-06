f53040be0d6aaedb12491b1467d3bcfbd289d491
AsyKL <kozmovskaya.as@gmail.com>
2024-12-06 14:50:30 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+import fake_math as fm
+import true_math as tm
+result1 = fm.divide_f(69, 3)
+result2 = fm.divide_f(3, 0)
+result3 = tm.divide_t(49, 7)
+result4 = tm.divide_t(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
